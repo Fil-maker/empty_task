8785c81df6b7a4b779b7d0463f811d1c407e56d0
zubar <zubareff10@gmail.com>
2022-12-23 23:39:30 +0500

    3.3.2 Задача с API ЦБ (несложная)

--- main.py
+++ main.py
@@ -25,14 +25,10 @@ def create_dic_cur(date):
 
 # region fast
 
-def calculate_mini(filename, speciality, year, cur_part, q):
+def calculate_mini(filename, q):
     first = False
-    am = 0
-    avg_s = 0
-    alt_am = 0
-    alt_avg_s = 0
     mont_curs = {}
-    val_ocs = {}
+    vacs = []
     with open(filename, encoding="utf-8") as file:
         reader = csv.reader(file)
         for row in reader:
@@ -42,35 +38,23 @@ def calculate_mini(filename, speciality, year, cur_part, q):
                 SALARY_FROM = row.index("salary_from")
                 SALARY_TO = row.index("salary_to")
                 SALARY_CURRENCY = row.index("salary_currency")
+                AREA_NAME = row.index("area_name")
                 PUBLISHED_AT = row.index("published_at")
             else:
                 if mont_curs.get(row[PUBLISHED_AT][:7], None) is None:
                     mont_curs[row[PUBLISHED_AT][:7]] = create_dic_cur(row[PUBLISHED_AT][:7])
                 currency_to_rub = mont_curs[row[PUBLISHED_AT][:7]]
-                if row[SALARY_CURRENCY] in currency_to_rub.keys() and \
-                        cur_part.get(row[SALARY_CURRENCY], 0) > 5000 \
-                        and row[SALARY_CURRENCY] not in [None, '']:
-                    # am += 1
-                    if val_ocs.get(row[PUBLISHED_AT][:7], None) is None:
-                        val_ocs[row[PUBLISHED_AT][:7]] = {}
-                    val_ocs[row[PUBLISHED_AT][:7]][row[SALARY_CURRENCY]] = \
-                        val_ocs.get(row[PUBLISHED_AT][:7], {}).get(row[SALARY_CURRENCY], 0) + 1
-                    # if row[SALARY_FROM] in [None, ''] and row[SALARY_TO] not in [None, '']:
-                    #     row[SALARY_FROM] = row[SALARY_TO]
-                    # if row[SALARY_TO] in [None, ''] and row[SALARY_FROM] not in [None, '']:
-                    #     row[SALARY_TO] = row[SALARY_FROM]
-                    # avg_s += (num(row[SALARY_FROM]) + num(row[SALARY_TO])) / 2 * currency_to_rub[row[SALARY_CURRENCY]]
-                    # if speciality in row[NAME]:
-                    #     alt_am += 1
-                    #     alt_avg_s += (num(row[SALARY_FROM]) + num(row[SALARY_TO])) / 2 * currency_to_rub[
-                    #         row[SALARY_CURRENCY]]
-    # av1, av2 = 0, 0
-    # if am != 0:
-    #     av1 = int(avg_s // am)
-    # if alt_am != 0:
-    #     av2 = int(alt_avg_s // alt_am)
-    # q.put((year, am, av1, alt_am, av2))
-    q.put(val_ocs)
+                if row[SALARY_FROM] in [None, ''] and row[SALARY_TO] not in [None, ''] or SALARY_CURRENCY is None:
+                    row[SALARY_FROM] = row[SALARY_TO]
+                if row[SALARY_TO] in [None, ''] and row[SALARY_FROM] not in [None, '']:
+                    row[SALARY_TO] = row[SALARY_FROM]
+                if row[SALARY_FROM] == '' and row[SALARY_TO] == '' or row[SALARY_CURRENCY] == '' or row[SALARY_CURRENCY] not in currency_to_rub.keys():
+                    salary = None
+                else:
+
+                    salary = (float(row[SALARY_FROM]) + float(row[SALARY_TO])) * currency_to_rub[row[SALARY_CURRENCY]]
+                vacs.append([row[NAME], salary, row[AREA_NAME], row[PUBLISHED_AT]])
+    q.put(vacs)
 
 
 def split_file(filename):
@@ -102,7 +86,7 @@ def split_file(filename):
     return list(mini_files.keys()), currencies
 
 
-def calc_multi(years, mypath, spec, curs):
+def calc_multi(years, mypath):
     onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
     procs = {}
     q = Queue()
@@ -112,23 +96,18 @@ def calc_multi(years, mypath, spec, curs):
         #     ex = executor.submit(calculate_mini, f"splits\\split_{year}.csv", spec, year)
         #     procs.append(ex)
         for year in years:
-            proc = Process(target=calculate_mini, args=(f"splits\\split_{year}.csv", spec, year, curs, q))
+            proc = Process(target=calculate_mini, args=(f"splits\\split_{year}.csv", q))
             procs[year] = proc
             proc.start()
-    g_curs = list(filter(lambda i: curs[i] > 5000 and i != '', curs.keys()))
+
     apps = []
     indexes = []
     for i in range(len(years)):
         # data = procs[i].result()
         data = q.get()
-        for key, val in data.items():
-            cort = []
-            for c in g_curs:
-                cort.append(val.get(c, 0) / sum(val.values()))
-            apps.append(cort)
-            indexes.append(key)
-    frame = pd.DataFrame(apps, columns=g_curs, index=indexes)
-    frame.index.name = 'date'
+        apps = apps + data
+    frame = pd.DataFrame(apps, columns=['name', 'salary', 'area_name', 'published_at'])
+    frame.set_index('name', inplace=True)
     frame.to_csv("result.csv")
 
 
@@ -142,7 +121,7 @@ if __name__ == '__main__':
     filename, spec = "vacancies/vacancies_dif_currencies.csv", "Программист"
     # years, curs = split_file(filename)
     years = range(2003, 2023)
-    curs = {'': 1928667, 'USD': 167994, 'RUR': 1830967, 'EUR': 10641, 'KZT': 65291, 'UAH': 25969, 'BYR': 41178,
-            'AZN': 607, 'UZS': 2966, 'KGS': 645, 'GEL': 36}
+    # curs = {'': 1928667, 'USD': 167994, 'RUR': 1830967, 'EUR': 10641, 'KZT': 65291, 'UAH': 25969, 'BYR': 41178,
+    #         'AZN': 607, 'UZS': 2966, 'KGS': 645, 'GEL': 36}
     # print(curs)
-    calc_multi(years, 'splits', spec, curs)
+    calc_multi(years, 'splits')
